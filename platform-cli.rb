98022427cbb0a671b77b480cb544a049c22ed9ba
Marcos Antonio Lopes <montu@mabil.co>
2024-03-15 17:43:51 +1000

    update platform-cli to 0.3.0

--- platform-cli.rb
+++ platform-cli.rb
@@ -4,8 +4,8 @@ require_relative "lib/private_strategy"
 class PlatformCli < Formula
   desc "Platform Utilities"
   homepage "https://github.com/MontuGroup/platform-cli"
-  url "https://github.com/MontuGroup/platform-cli/releases/download/0.2.0/m.aarch64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "b64f864f2b6a56e916709b27a6e3f8cf5a22b11195dda4db8d8f885891b0ec81"
+  url "https://github.com/MontuGroup/platform-cli/releases/download/0.3.0/m.aarch64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "5bb1157539a9f56c57a80851abc1bb532facc5ca877fcfd6b1f1b2520dde25f2"
   head "https://github.com/MontuGroup/platform-cli.git"
 
   def install
